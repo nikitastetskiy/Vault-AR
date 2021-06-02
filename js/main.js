043b7e9eb3a3ff662a9484fef404ce69fb7178c1
Nikita Stetskiy <nikin929@gmail.com>
2021-06-02 20:51:33 +0200

    Adding js files

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function() {
+
+	$('.owl-1').owlCarousel({
+
+    loop:true,
+    margin:0,
+    nav:true,
+    items: 1,
+    mouseDrag: false,
+    touchDrag: false,
+    smartSpeed: 1000,
+    autoplay: false,
+    checkVisible: false,
+    autoplayHoverPause: false,
+    navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+	})
+
+	
+})
